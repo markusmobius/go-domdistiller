7f8381c42211cd8342920b2e9078aab472e7b46c
Radhi Fadlillah <m.radhi.f@gmail.com>
2020-10-14 06:37:52 +0700

    Rename filter

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -4,8 +4,8 @@ package filter
 
 import "github.com/markusmobius/go-domdistiller/internal/webdoc"
 
-// Filter is interface for filter that process a TextDocument.
-type Filter interface {
+// TextDocumentFilter is interface for filter that process a TextDocument.
+type TextDocumentFilter interface {
 	// Process processes the given document.
 	// Returns true if changes have been made to the document.
 	Process(doc *webdoc.TextDocument) bool
