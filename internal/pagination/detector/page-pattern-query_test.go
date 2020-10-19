4adc545d282f0acf93b17121d8187ff33e56ffdd
Radhi Fadlillah <m.radhi.f@gmail.com>
2020-10-19 10:13:13 +0700

    Tidy up test a bit

--- internal/pagination/detector/page-pattern-query_test.go
+++ internal/pagination/detector/page-pattern-query_test.go
@@ -11,79 +11,79 @@ import (
 )
 
 func Test_Pagination_Detector_QPPP_IsPagingURL(t *testing.T) {
-	assert.True(t, qpppIsPagingURL(t,
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryA=v1&queryB=4&queryC=v3",
-		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryA=v1&queryB=growl&queryB=5&queryC=v3",
-		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryA=v1&queryC=v3",
-		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryB=2&queryC=v3",
-		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryC=v3",
-		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?queryC=v3&queryC=v4",
-		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3&queryC=v4"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3&queryC=v4")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b",
-		"http://www.foo.com/a/b?page=[*!]"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?page=[*!]")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b?page=3",
-		"http://www.foo.com/a/b?page=[*!]"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?page=[*!]")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b/",
-		"http://www.foo.com/a/b?page=[*!]"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?page=[*!]")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b.htm",
-		"http://www.foo.com/a/b?page=[*!]"))
-	assert.True(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?page=[*!]")
+	qpppAssertPagingURL(t, true,
 		"http://www.foo.com/a/b.html",
-		"http://www.foo.com/a/b?page=[*!]"))
-	assert.False(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?page=[*!]")
+	qpppAssertPagingURL(t, false,
 		"http://www.foo.com/a/b?queryA=v1&queryC=v3",
-		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3"))
-	assert.False(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, false,
 		"http://www.foo.com/a/b?queryB=bar&queryC=v3",
-		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3"))
-	assert.False(t, qpppIsPagingURL(t,
+		"http://www.foo.com/a/b?queryB=[*!]&queryC=v3")
+	qpppAssertPagingURL(t, false,
 		"http://www.foo.com/a/b?queryA=v1",
-		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3"))
+		"http://www.foo.com/a/b?queryA=v1&queryB=[*!]&queryC=v3")
 }
 
 func Test_Pagination_Detector_QPPP_IsPagePatternValid(t *testing.T) {
-	assert.True(t, qpppIsPagePatternValid(t,
+	qpppAssertPagePatternValid(t, true,
 		"http://www.google.com/forum-12",
-		"http://www.google.com/forum-12?page=[*!]"))
-	assert.True(t, qpppIsPagePatternValid(t,
+		"http://www.google.com/forum-12?page=[*!]")
+	qpppAssertPagePatternValid(t, true,
 		"http://www.google.com/forum-12?sid=12345",
-		"http://www.google.com/forum-12?page=[*!]&sort=d"))
-	assert.False(t, qpppIsPagePatternValid(t,
+		"http://www.google.com/forum-12?page=[*!]&sort=d")
+	qpppAssertPagePatternValid(t, false,
 		"http://www.google.com/a/forum-12?sid=12345",
-		"http://www.google.com/b/forum-12?page=[*!]&sort=d"))
-	assert.False(t, qpppIsPagePatternValid(t,
+		"http://www.google.com/b/forum-12?page=[*!]&sort=d")
+	qpppAssertPagePatternValid(t, false,
 		"http://www.google.com/forum-11?sid=12345",
-		"http://www.google.com/forum-12?page=[*!]&sort=d"))
+		"http://www.google.com/forum-12?page=[*!]&sort=d")
 }
 
-func qpppIsPagingURL(t *testing.T, strURL string, strPattern string) bool {
+func qpppAssertPagingURL(t *testing.T, expected bool, strURL string, strPattern string) {
 	pattern := createQueryParamPagePattern(strPattern)
 	assert.NotNil(t, pattern)
-	return pattern.IsPagingURL(strURL)
+	assert.Equal(t, expected, pattern.IsPagingURL(strURL))
 }
 
-func qpppIsPagePatternValid(t *testing.T, strURL string, strPattern string) bool {
+func qpppAssertPagePatternValid(t *testing.T, expected bool, strURL string, strPattern string) {
 	parsedURL, _ := nurl.ParseRequestURI(strURL)
 	assert.NotNil(t, parsedURL)
 
 	pattern := createQueryParamPagePattern(strPattern)
 	assert.NotNil(t, pattern)
 
-	return pattern.IsValidFor(parsedURL)
+	assert.Equal(t, expected, pattern.IsValidFor(parsedURL))
 }
 
 func createQueryParamPagePattern(strPattern string) detector.PagePattern {
